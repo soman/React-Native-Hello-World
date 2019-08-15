0626d486f2f962d22d7539eca718e884454bd6c5
Soman Sarker <soman.mia@sourcetop.com>
2019-08-15 09:41:36 +0600

    Used native base

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, View, Text } from 'native-base';
 export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <View style={{ flexDirection: "row", flex: 1, justifyContent: 'center', marginTop: 100 }}>
+          <Text>
+            Hello World!
+          </Text>
+          </View>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
